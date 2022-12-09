b4354c90a439f668924b83ba398a608d7e8c1728
nguyen <nguyenngocnguyen.rtc@gmail.com>
2022-12-10 00:10:02 +0700

    Update CategoryBranch, Category FK

--- DataAccess/Repositories/IRepositories/ICategoryBranch.cs
+++ DataAccess/Repositories/IRepositories/ICategoryBranch.cs
@@ -3,7 +3,7 @@ using Models;
 
 namespace DataAccess.Repositories.IRepositories
 {
-    public interface ICategoryBranch : IRepository<CategoriesBranches>
+    public interface ICategoryBranch : IRepository<CategoryBranch>
     {
         void Update(Branch obj);
     }
--- Models/Category.cs
+++ Models/Category.cs
@@ -9,10 +9,8 @@ namespace Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         [StringLength(255)]
-        public string Image { get; set; }
-        public string UserId { get; set; }
-        [ForeignKey("UserId")]
-        public virtual User User { get; set; }
+        public string Image { get; set; }        
+
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; set; }
     }
