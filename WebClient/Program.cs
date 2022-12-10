0c3cf9c505055ecac70bc0f29cbe1d8abcffa9c0
nguyen <nguyenngocnguyen.rtc@gmail.com>
2022-12-11 02:22:44 +0700

    a

--- WebClient/Program.cs
+++ WebClient/Program.cs
@@ -26,8 +26,8 @@ builder.Services.AddDefaultIdentity<User>(options => {
     options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
 
     // Cấu hình Lockout - khóa user
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
-    options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(30); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 5 lầ thì khóa
     options.Lockout.AllowedForNewUsers = true;
 
     // Cấu hình về User.
