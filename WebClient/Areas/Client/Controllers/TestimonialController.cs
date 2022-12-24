2064c0f99328605d3053a74b1a162579c8132c37
ducth <hongduc109>
2022-12-25 03:04:07 +0700

    Update testimonial client paging.

--- WebClient/Areas/Client/Controllers/TestimonialController.cs
+++ WebClient/Areas/Client/Controllers/TestimonialController.cs
@@ -1,21 +1,40 @@
-﻿using DataAccess.Repositories.IRepositories;
+﻿using DataAccess.Data;
+using DataAccess.Repositories.IRepositories;
+using DataAccess.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebClient.Areas.Client.Controllers
 {
     [Area("Client")]
     public class TestimonialController : Controller
     {
-        private readonly IUnitOfWork unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly StarSecurityDbContext _context;
 
-        public TestimonialController(IUnitOfWork unitOfWork)
+        public TestimonialController(IUnitOfWork unitOfWork, StarSecurityDbContext context)
         {
-            this.unitOfWork = unitOfWork;
+            this._unitOfWork = unitOfWork;
+            this._context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int p = 1)
         {
-            return View();
+            try
+            {
+                var model = await _unitOfWork.Testimonial.GetAll();
+
+                int pageSize = 6;
+                ViewBag.PageNumber = p;
+                ViewBag.PageRange = pageSize;
+                ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Categories.Count() / pageSize);
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Error", new { area = "Admin" });
+            }
         }
     }
 }
